2b2fbfb2ca4a4105f60fda056fbb6d3a31285ca8
dhtveso <cska48sofia@gmail.com>
2017-06-05 13:01:11 +0300

    Added class Mobs

---
+++ AlphaBomberman.Models/Mobs.cs
@@ -0,0 +1,11 @@
+﻿namespace AlphaBomberman.Models
+{
+    using System;
+
+    public class Mobs
+    {
+        public int Healt { get; set; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+    }
+}
