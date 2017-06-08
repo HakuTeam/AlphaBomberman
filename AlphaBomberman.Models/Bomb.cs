0e984b23e0b2ab52f0ca3410bcc958fe9e488624
Plotso <ilian_1999n@abv.bg>
2017-06-09 00:48:28 +0300

    Added the class Bomb (initial edition)

---
+++ AlphaBomberman.Models/Bomb.cs
@@ -0,0 +1,13 @@
+﻿namespace AlphaBomberman.Models
+{
+    using System;
+
+    class Bomb
+    {
+        public int Power;
+        public int Range;
+        public Player Owner;
+        public Mobs Holder;
+        //Not quite sure whether to put it with the mobs or the player so I decided both 
+    }
+}
