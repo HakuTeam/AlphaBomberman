7698db65e07a35e465ecb27c312464ea015dc13d
Zlatyo Uzunov <zluzunov@gmail.com>
2017-06-08 12:20:06 +0300

    Added Utilities Class Library; Added Ennumeration class for commands; Added KeyboardInput class for keys in the game; these classes are imported from the DB fundamentals project: https://github.com/MetaDevTeam/MetaPong We can use them for the object in the game. More imported classes to come ...

---
+++ AlphaBomberman.Utilities/Ennumetation/Command.cs
@@ -0,0 +1,22 @@
+﻿namespace AlphaBomberman.Utilities.Ennumetation
+{
+    /// <summary>
+    /// Ennumeration object used for menu commands</summary>
+    /// <remarks>
+    /// TODO - add example here 
+    /// </remarks>
+    public enum Command
+    {
+        Unknown,
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        PlaceBomb,
+        Execute,
+        StartGame,
+        Options,
+        HomeScreen,
+        Exit
+    }
+}
---
+++ AlphaBomberman.Utilities/Input/KeyboardInput.cs
@@ -0,0 +1,52 @@
+﻿namespace AlphaBomberman.Utilities.Input
+{
+    using System;
+    using Ennumetation;
+
+    /// <summary>
+    /// KeyboardInput class contains commads for listening the
+    /// user input</summary>
+    /// <remarks>
+    /// TODO - add example or notes here 
+    /// </remarks>
+    public class KeyboardInput
+    {
+        /// <summary>
+        /// Listens until a key is pressed and translates it 
+        /// to the corresponding command.
+        /// </summary>
+        /// <remarks>
+        /// If you need a new commad, first add it to the
+        /// Command class.
+        /// </remarks>
+        public Command Listen()
+        {
+            var next = Console.ReadKey(true);
+            switch (next.Key.ToString())
+            {
+                case "UpArrow":
+                    return Command.MoveUp;
+                case "DownArrow":
+                    return Command.MoveDown;
+                case "LeftArrow":
+                    return Command.MoveLeft;
+                case "RightArrow":
+                    return Command.MoveRight;
+                case "NumPad0":
+                    return Command.PlaceBomb;
+                case "Enter":
+                    return Command.Execute;
+                case "Escape":
+                    return Command.Exit;
+                default:
+                    return Command.Unknown;
+            }
+        }
+
+        public string Read()
+        {
+            var input = Console.ReadLine();
+            return input;
+        }
+    }
+}
